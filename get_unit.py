43dfcb64670dde0ea7a2bf1934457c77f2a1b411
Leed0251 <99302462+Leed0251@users.noreply.github.com>
2023-06-22 12:53:32 +1200

    Add files via upload

--- get_unit.py
+++ get_unit.py
@@ -1,27 +1,32 @@
+import configparser
+
 def get_unit(question):
     # Dictionary is ordered by unit length and value shows multiple
-    units = {
-        "kg": 1000,
-        "ml": 1,
-        "l": 1000,
-        "g": 1,
-    }
-    
+    parser = configparser.ConfigParser()
+    parser.read("units.ini")
+    units = parser._sections["Units"]
+
+    weight_error = "Input must be a valid measurement of weight"
+    boundary_error = "Unit must be higher than 0"
+
     while True:
         try:
             user_input = input(question)
+            # Loop through the units to find what was given
             for unit in units:
                 length = len(unit)
                 if (user_input[-length:]).lower() == unit.lower():
+                    # Seperate the number from the unit
                     num = user_input[:-length]
                     if float(num) > 0:
+                        # Makes sure the unit has a proper value
                         if num == "":
-                            raise ValueError("Input must be a valid measurement of weight")
+                            raise ValueError(weight_error)
                         user_number = float(num)
                     else:
-                        raise ValueError("Unit must be higher than 0")
+                        raise ValueError(boundary_error)
 
-                    return user_number * units[unit]
-            raise ValueError("Input must be a valid measurement of weight")
+                    return user_number * float(units[unit])
+            raise ValueError(weight_error)
         except ValueError as error:
             print(error)
